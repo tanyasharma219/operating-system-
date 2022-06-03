1a3856acd28eb59a25f29157dd93a3f5f78b0479
tanyasharma219 <63143037+tanyasharma219@users.noreply.github.com>
2022-06-03 22:38:59 +0530

    Create Experiment_7.3.sh

---
+++ exp7/Experiment_7.3.sh
@@ -0,0 +1,11 @@
+ines (8 sloc)  173 Bytes
+   
+# Delete the lines containing a specific word
+
+if [ $# -lt 1 ]; then
+	echo 'Check arguments once'
+else
+	for file in *.txt; do
+		sed -i '/$1/d' ./file > ./'new'$file
+	done
+fi
